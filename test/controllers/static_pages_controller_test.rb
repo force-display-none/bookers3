7aeba1a4c3f5534f2ae4be034a1bc5e859246e27
kckw <kckw0917@gmail.com>
2018-07-14 08:03:27 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Bookers"
+  end
+
   test "should get top" do
     get static_pages_top_url
     assert_response :success
+    assert_select "title", "Top | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
