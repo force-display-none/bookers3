281547a5f3da0aa075c02af57698e9ba4006fb3c
kckw <kckw0917@gmail.com>
2018-07-15 08:01:18 +0000

    Add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'static_pages#top'
   get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
