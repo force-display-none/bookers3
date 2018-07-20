b89ad005a69f4f1065d0aa5db3a411c22eb85ca5
kckw <kckw0917@gmail.com>
2018-07-20 04:58:58 +0000

    Add BookComments controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
 
   validates_presence_of :title
   validates_presence_of :body
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   attachment :image
 
   validates_uniqueness_of :name
