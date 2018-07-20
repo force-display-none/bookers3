26036ac6a270c1bcf63b67e32cbbec8a283670a5
kckw <kckw0917@gmail.com>
2018-07-20 11:08:06 +0000

    Finish comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/about', to: 'static_pages#about'
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+  	resources :book_comments, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
