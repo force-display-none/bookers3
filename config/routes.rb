76ad3ece84062907f439baad085f7767279259b7
kckw <kckw0917@gmail.com>
2018-07-23 13:09:03 +0000

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#top'
   get '/about', to: 'static_pages#about'
+
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
   	resources :book_comments, only: [:create, :destroy]
